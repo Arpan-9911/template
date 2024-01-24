410cd5cfc488674b2fb77607ae58a610ab51a6e0
Arpan-9911 <arpanks1263@gmail.com>
2024-01-24 12:15:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="dkenterprises.netlify.app" target="_blank">Youtube</a>
+          <a href="dkenterprises.netlify.app" target="_blank">Instagram</a>
+          <a href="dkenterprises.netlify.app" target="_blank">Github</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
